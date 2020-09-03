a99a0965d0091f037cfbe17bb34bce588b74daac
dredance <zbigniew.figaj@gmail.com>
2020-09-03 10:33:33 +0200

    add

--- api/services.py
+++ api/services.py
@@ -1,42 +1,4 @@
-from io import BytesIO
-import base64
-import matplotlib.pyplot as plt
-import matplotlib.dates
-from matplotlib.dates import DateFormatter, MonthLocator
 import datetime as dt
-import pandas as pd
-from django.db.models import Max, Min
-
-
-class ServiceChart:
-    @staticmethod
-    def create_date(date):
-        """Creates the date"""
-        try:
-            year, month, day = date.split('-')
-            date = dt.datetime(int(year), int(month), int(day))
-        except:
-            pass
-        finally:
-            mdate = matplotlib.dates.date2num(date)
-        return mdate
-
-    @staticmethod
-    def get_image():
-        # create a bytes buffer for the image to save
-        buffer = BytesIO()
-        # create the plot with the use of BytesIO object as its 'file'
-        plt.savefig(buffer, format='png')
-        # set the cursor the begining of the stream
-        buffer.seek(0)
-        # retreive the entire content of the 'file'
-        image_png = buffer.getvalue()
-        graph = base64.b64encode(image_png)
-        graph = graph.decode('utf-8')
-        # free the memory of the buffer
-        buffer.close()
-        return graph
-
 
 class Service:
     @staticmethod
