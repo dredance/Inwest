b339d3a6049f8d96d39f45db3d18a0660c5128c8
dredance <zbigniew.figaj@gmail.com>
2020-09-05 12:58:25 +0200

    change urls

--- api/urls.py
+++ api/urls.py
@@ -9,13 +9,12 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path(r'okresy/', views.OkresyView.as_view(), name='okresy'),
-    path(r'obiekty/',views.ObiektyView.as_view(),name='obiekty'),
+    path(r'obiekty/',views.ObiektyView.as_view(),name='ob'),
     path(r'analiza/', views.AnalizaProjektuView.as_view(), name='analiza'),
-    path(r'sl/', views.SpawanieLinioweView.as_view(), name='sl'),
-    path(r'sm/', views.SpawanieMontazoweView.as_view(), name='sm'),
-    path(r'uk/', views.UkladkaView.as_view(), name='uk'),
-    path(r'mb/', views.MetodyBezwykopoweView.as_view(), name='mb'),
-    path(r'ob/', views.ObiektyView.as_view(), name='ob'),
+    path(r'spawanie_liniowe/', views.SpawanieLinioweView.as_view(), name='sl'),
+    path(r'spawanie_montaz/', views.SpawanieMontazoweView.as_view(), name='sm'),
+    path(r'ukladka/', views.UkladkaView.as_view(), name='uk'),
+    path(r'metody_bezwykopowe/', views.MetodyBezwykopoweView.as_view(), name='mb'),
 
     path(r'manager_data/', views.ManagerDataView.as_view(), name='manager-data'),
     path(r'new/<int:psp>/<slug:okres>/<slug:prev_data>/', views.CreateMultiFormView.as_view(), name='multiform-create'),
